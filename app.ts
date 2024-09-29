6d3f0b3f72585f20f470dc62a1516766c6321761
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-09-29 18:43:21 +0900

    object型

--- app.ts
+++ app.ts
@@ -1,16 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean ,Phrase: string) {
-    const result = n1 + n2
-    if(showResult) {
-        console.log(Phrase + result);
-    }else{
-        return n1 + n2
-    }
-}
+// const person: {
+//     name: string,
+//     age: number,
+// } 
 
-let number1 = 5;
-number1 = 'Hello';
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result'
+const person = {
+    name: 'yota',
+    age: 30,
+};
 
-add(number1, number2,printResult, resultPhrase)
+console.log(person.name);
