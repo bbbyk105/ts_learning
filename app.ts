943de519480e81322e3dbdb1e65cd5fd88560a7e
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-01 15:52:14 +0900

    enum型

--- app.ts
+++ app.ts
@@ -1,24 +1,41 @@
-// const person: {
-//     name: string,
-//     age: number,
-// } 
+// // const person: {
+// //     name: string,
+// //     age: number,
+// // } 
 
-const person: {
-    name:string;
-    age: number;
-    hobbies: string[];
-    role:[number, string]
-} = {
+// const person: {
+//     name:string;
+//     age: number;
+//     hobbies: string[];
+//     role:[number, string]
+// } = {
+//     name: 'yota',
+//     age: 30,
+//     hobbies:['Sports', 'Cooking'],
+//     role: [2, 'author'],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+    ADMIN, 
+    READ_ONLY,
+    AUTHOR,
+}
+
+const person={
     name: 'yota',
     age: 30,
     hobbies:['Sports', 'Cooking'],
-    role: [2, 'author'],
+    role: Role.ADMIN,
 };
 
-// person.role.push('admin');
-// person.role[1] = 10;
+// // person.role.push('admin');
+// // person.role[1] = 10;
 
-// person.role = [0, 'admin', 'user'];
+// // person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 favoriteActivities =['Sports'];
@@ -28,4 +45,9 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
 }
+
+if(person.role === Role.ADMIN) {
+    console.log("管理者ユーザー");
+}
+
 
