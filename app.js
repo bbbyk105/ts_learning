4a1a63ad07826cc14831984474a9ae53765eb8c4
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-05 20:14:29 +0900

    function型

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result' + num);
 }
-console.log(printResult(add(5, 12)));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+console.log(combineValues(8, 8));
+printResult(add(5, 12));
--- app.ts
+++ app.ts
@@ -6,4 +6,11 @@ function printResult(num: number){
     console.log('Result' + num);
 }
 
+let combineValues: (a:number , b:number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+
+console.log(combineValues(8, 8))
+
 printResult(add(5, 12));
