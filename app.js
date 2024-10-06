89c7cf1339ec782562349b8ac4e1f281fcedd39a
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-06 18:00:32 +0900

    tsの管理

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-var userInput;
-var userName;
-userInput = 5;
-userName = "Max";
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-var result = generateError('エラーが発生しました。', 500);
-console.log(result);
+"use strict";
