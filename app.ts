14ff36129d688c02f31ed97bdb3dc432a829c745
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-06 17:44:02 +0900

    never型

--- app.ts
+++ app.ts
@@ -7,3 +7,10 @@ userName = "Max";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number) {
+    throw {message: message, errorCode: code }
+}
+
+const result = generateError('エラーが発生しました。', 500);
+console.log(result);
